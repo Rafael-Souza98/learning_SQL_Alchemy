e8b86374d7b6cdea2ded8cbcd0f9b5fb60fd0a3c
Rafael Souza <faelsos98@gmail.com>
2022-09-12 10:29:18 -0300

    add error handling

--- learning_sqlalchemy/infra/repository/filmes_repository.py
+++ learning_sqlalchemy/infra/repository/filmes_repository.py
@@ -1,29 +1,48 @@
 from infra.configs.connection import DBConnetionHandler
 from infra.entities.filmes import Filmes
 from sqlalchemy import delete, update
+from sqlalchemy.orm.exc import NoResultFound
 
 class FilmesRepository:
     def select(self):
         with DBConnetionHandler() as db:
-            data = db.session.query(Filmes).all()
-            return data
-        
+           try:
+                data = db.session.query(Filmes).all()
+                return data
+           except Exception as exception:
+               return None
+           except Exception as exception:
+                 db.session.rollback()
+                 raise exception
+           
     def insert(self, titulo, genero, ano):
         with DBConnetionHandler() as db:
-            insert_filme = Filmes(titulo=titulo, genero=genero, ano=ano)
-            db.session.add(insert_filme)
-            db.session.commit()
-    
+            try:
+                insert_filme = Filmes(titulo=titulo, genero=genero, ano=ano)
+                db.session.add(insert_filme)
+                db.session.commit()
+            except Exception as exception:
+                 db.session.rollback()
+                 raise exception
+        
     def update (self, titulo, ano):
-        with DBConnetionHandler() as db:
-            update(Filmes).where(Filmes.titulo == titulo).values(ano = ano)
-            db.session.commit()
+        try:
+            with DBConnetionHandler() as db:
+                update(Filmes).where(Filmes.titulo == titulo).values(ano = ano)
+                db.session.commit()
+        except Exception as exception:
+                 db.session.rollback()
+                 raise exception
         
     def delete (self, titulo):
-        with DBConnetionHandler() as db:
-            delete_filme = delete(Filmes).where(Filmes.titulo == titulo)
-            db.session.execute(delete_filme)
-            db.session.commit()
+        try:
+            with DBConnetionHandler() as db:
+                delete_filme = delete(Filmes).where(Filmes.titulo == titulo)
+                db.session.execute(delete_filme)
+                db.session.commit()
+        except Exception as exception:
+                 db.session.rollback()
+                 raise exception
             
             
             
